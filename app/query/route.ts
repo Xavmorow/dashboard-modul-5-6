a6e86d27f652888ace83d54f2f1bb33969b794e2
xavmorow <241713125@students.uajy.ac.id>
2026-05-09 11:58:13 +0700

    update modul 7-9

--- app/query/route.ts
+++ app/query/route.ts
@@ -16,7 +16,7 @@ async function listInvoices() {
 export async function GET() {
   try {
   	return Response.json(await listInvoices());
-  } catch (error) {
-  	return Response.json({ error }, { status: 500 });
+  } catch (error: any) {
+  	return Response.json({ error: error.message }, { status: 500 });
   }
 }
