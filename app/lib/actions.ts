6e2ea948b8bf3f1c867f1013ffe864b82804d243
xavmorow <241713125@students.uajy.ac.id>
2026-05-21 10:44:03 +0700

    update 11

---
+++ app/lib/actions.ts
@@ -0,0 +1,11 @@
+'use server';
+
+export async function createInvoice(formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  // Cek di inspect(F12) bagian console
+  console.log(rawFormData);
+}
